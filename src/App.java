4b49b3e5fcbee1c31eb4339102a1f4debaf586dd
William Horn <williamjosephhorn@gmail.com>
2025-04-06 16:21:21 -0400

    INITIAL dev branch

--- src/App.java
+++ src/App.java
@@ -4,7 +4,6 @@
  */
 
 import classes.abstracts.Application;
-import classes.entity.Game;
 import classes.util.Console;
 
 /**
@@ -17,15 +16,14 @@ public class App extends Application {
         // Console.hideDebugPriority(DebugPriority.LOW);
         Console.setConsoleColorsEnabled(true);
 
-        Game game = new Game();
-        game.start();
+        // Game game = new Game();
+        // game.start();
         /**
          * TEST CODE:
          */
         // Vector2 v0 = new Vector2(7, 3);
         // Vector2 v1 = new Vector2(-1, 1);
         // CellGrid grid = new CellGrid(new Unit2(10, 10));
-
         // Iterator<Cell> itr = grid.getCellPathIterator(v0, v1);
         // while (itr.hasNext()) {
         //     Console.println(itr.next());
