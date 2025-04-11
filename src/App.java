e5244dbcc275ccadf084d8b0fb9720297fa455a4
William Horn <williamjosephhorn@gmail.com>
2025-04-11 16:18:26 -0400

    STASH

--- src/App.java
+++ src/App.java
@@ -49,7 +49,7 @@ public class App {
 		Console.setDebugModeEnabled(true);
 		Console.hideDebugPriority(DebugPriority.LOW);
 		// Console.hideDebugPriority(DebugPriority.MEDIUM);
-		Console.setConsoleColorsEnabled(false);
+		Console.setConsoleColorsEnabled(true);
 
 		Game game = new Game();
 		CellGrid grid = game.getGameGrid();
